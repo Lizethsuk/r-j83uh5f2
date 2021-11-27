06c1d47731d681ff9e1547f76b143a9eaa6642d5
Lizeth <julia.sucasacai@gmail.com>
2021-11-26 20:17:45 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { value: 0 };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {    this.setState({value: event.target.value.length});  }
   render() {
+    
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea  onChange={this.handleChange} rows="3"  ></textarea>
+        <div value={this.state.value} className="counter">{this.state.value}</div>
       </div>
+      
     );
-  }
+  };
+  
+ 
 }
 
 export default App;
